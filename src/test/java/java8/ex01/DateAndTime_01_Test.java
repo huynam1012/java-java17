679275e0b28b1be6d30e1081117b5f4e5a1b577c
hntran <huynam1012@gmail.com>
2022-06-28 15:41:54 +0200

    TP date et time

--- src/test/java/java8/ex01/DateAndTime_01_Test.java
+++ src/test/java/java8/ex01/DateAndTime_01_Test.java
@@ -18,7 +18,7 @@ public class DateAndTime_01_Test {
 	public void test_date() {
 
 		// TODO modifier les paramètres du constructeur pour que le test soit passant
-		Date date = new Date(0, 0, 0, 0, 0, 0);
+		Date date = new Date(117, 2, 24, 1, 2, 3);
 
 		assertThat(date.toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
--- src/test/java/java8/ex02/DateAndTime_02_Test.java
+++ src/test/java/java8/ex02/DateAndTime_02_Test.java
@@ -5,7 +5,10 @@ import static org.junit.Assert.assertThat;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
 import java.time.temporal.UnsupportedTemporalTypeException;
 
 import org.junit.Test;
@@ -19,15 +22,15 @@ public class DateAndTime_02_Test {
 	public void test_localDate_of() {
 
 		// TODO créer un objet LocalDate à la date 24/12/2050
-		LocalDate result = null;
+		LocalDate result1 =  LocalDate.of(2050, 12, 24);
 
 		// TODO En exploitant les méthodes de l'objet result, valoriser les
 		// différentes variables afin de rendre le test passant.
-		int year = 0;
-		Month month = null;
-		int dayOfMonth = 0;
-		DayOfWeek dayOfWeek = null;
-		int dayOfYear = 0;
+		int year = result1.getYear();
+		Month month = result1.getMonth();
+		int dayOfMonth = result1.getDayOfMonth();
+		DayOfWeek dayOfWeek = result1.getDayOfWeek();
+		int dayOfYear = result1.getDayOfYear();
 
 		assertThat(year, is(2050));
 		assertThat(month, is(Month.DECEMBER));
@@ -41,13 +44,19 @@ public class DateAndTime_02_Test {
 
 		// TODO créer un objet LocalDate à la date 10/01/1990
 		// TODO utiliser la méthode parse
-		LocalDate result = null;
+		LocalDate result2 =  LocalDate.parse("1990-01-10"); // FORMAT ISO
+		
+		result2.getYear();
+		result2.getMonth();
+		result2.getDayOfMonth();
+		result2.getDayOfWeek();
+		result2.getDayOfYear();
 
 		// TODO En exploitant les méthodes de l'objet result, valoriser les
 		// différentes variables afin de rendre le test passant.
-		int year = 0;
-		Month month = null;
-		int dayOfMonth = 0;
+		int year = result2.getYear();
+		Month month = result2.getMonth();
+		int dayOfMonth = result2.getDayOfMonth();
 
 		assertThat(year, is(1990));
 		assertThat(month, is(Month.JANUARY));
@@ -59,12 +68,12 @@ public class DateAndTime_02_Test {
 
 		// TODO créer un objet LocalDate à la date 11/03/2015
 		// en utilisant la méthode of
-		LocalDate localDate = null;
+		LocalDate localDate =  LocalDate.of(2015, 3, 11);
 
 		// TODO Formatter la date pour que le test soit passant
-		String result = null;
+		String result3 = localDate.format(DateTimeFormatter.ofPattern("dd - MM - yyyy"));
 
-		assertThat(result, is("11 - 03 - 2015"));
+		assertThat(result3, is("11 - 03 - 2015"));
 	}
 
 	@Test(expected = UnsupportedTemporalTypeException.class)
@@ -72,10 +81,12 @@ public class DateAndTime_02_Test {
 
 		// TODO créer un objet LocalDate à la date 11/03/2015
 		// TODO utiliser la méthode of
-		LocalDate localDate = null;
+		LocalDate localDate = LocalDate.of(2015, 3, 11);
 
 		// TODO Formatter la date pour avoir l'affichage suivant : "11/03/2015 00:00:00"
-		localDate.format(null);
+		localDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+		
+		assertThat(localDate, is("11/03/2015 00:00:00"));
 	}
 
 	@Test
@@ -83,14 +94,14 @@ public class DateAndTime_02_Test {
 
 		// TODO créer un objet LocalDate à la date 10/01/2000
 		// TODO utiliser la méthode of
-		LocalDate localDate = null;
+		LocalDate localDate = LocalDate.of(2000, 01, 10);
 
 		// TODO transformer la date précédente en 05/02/2015
-		LocalDate result = null;
+		LocalDate result4 = localDate.plusYears(15).plusMonths(1).plusDays(-5);
 
-		assertThat(result.getYear(), is(2015));
-		assertThat(result.getMonth(), is(Month.FEBRUARY));
-		assertThat(result.getDayOfMonth(), is(5));
+		assertThat(result4.getYear(), is(2015));
+		assertThat(result4.getMonth(), is(Month.FEBRUARY));
+		assertThat(result4.getDayOfMonth(), is(5));
 	}
 
 }
